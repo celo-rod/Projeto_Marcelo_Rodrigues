0c7c1a5fc2c7f045981d5b7e1c56920ca6c6cec0
Marcelo Rodrigues <marcelorj280@gmail.com>
2023-10-02 16:12:36 -0300

    Criação da classe D

---
+++ src/ClasseD.java
@@ -0,0 +1,11 @@
+public class ClasseD{
+    private float d1;
+    private boolean d2;
+
+    public void md1(){
+        System.out.println(this.getClass().getSimpleName());
+    }
+    public void md2(){
+        System.out.println(this.getClass().getSimpleName());
+    }
+}
