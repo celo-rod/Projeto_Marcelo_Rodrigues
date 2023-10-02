56165a0a124fc1fb3191dce8974236b9a2eb36b6
Marcelo Rodrigues <marcelorj280@gmail.com>
2023-10-02 15:39:50 -0300

    Criação da classe C

---
+++ src/ClasseC.java
@@ -0,0 +1,11 @@
+public class ClasseC {
+    private String c1;
+    private int c2;
+
+    public void mc1(){
+        System.out.println(this.getClass().getSimpleName());
+    }
+    public void mc2(){
+        System.out.println(this.getClass().getSimpleName());
+    }
+}
